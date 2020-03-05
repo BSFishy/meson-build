6f208031248860c97efb473e4132097f4e886b74
BSFishy <mattprovost6@gmail.com>
2020-03-05 17:27:44 +0000

    Add one test

---
+++ __tests__/meson-build.test.ts
@@ -0,0 +1,10 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as path from 'path';
+
+var hasbin = require('hasbin');
+import main = require('../src/main');
+
+describe('meson-build', () => {
+    it('', () => {});
+});
